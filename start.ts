cd4e63209fc5acbe47daad14bc37a3cd643eb511
Wizard Today <aleshko.vitali@gmail.com>
2023-01-22 19:52:56 +0300

    add content type header

--- start.ts
+++ start.ts
@@ -1,5 +1,9 @@
 import { serve } from "https://deno.land/std/http/mod.ts"
 
 serve((_, conn) => (
-  new Response(JSON.stringify(conn))
+  new Response(JSON.stringify(conn), {
+    headers: new Headers({
+      'Content-Type': 'application/json; charset=utf-8'
+    })
+  })
 ))
